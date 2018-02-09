3734f7ade23ca8d428ca36f53be6664ca6f748cc
kiyology <jingxi@xperception.io>
2018-02-09 16:25:13 +0800

    Add missing header

---
+++ firmware/include/xp_sensor_firmware_version.h
@@ -0,0 +1,11 @@
+#ifndef __XP_SENSOR_FIRMWARE_VERSION_H__
+#define __XP_SENSOR_FIRMWARE_VERSION_H__
+
+#define FIRMWARE_VERSION "V0.6.5-366de54-commit"
+
+#define COMPILE_BY "jingxi"
+#define COMPILE_HOST "jingxi-Lenovo-ideapad-FLEX-4-1570"
+#define COMPILE_TIME "16:24:30"
+#define COMPILE_DATE "2018-02-09"
+
+#endif
